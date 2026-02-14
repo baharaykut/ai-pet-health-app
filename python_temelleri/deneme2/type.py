4ac1b80230817ae0f988202220fb784b9c7586ef
Bahar Aykut <bbaharaykut@gmail.com>
2026-02-14 21:32:12 +0300

    remove JWT and Google secrets from config

---
+++ python_temelleri/deneme2/type.py
@@ -0,0 +1,20 @@
+'''x=input()
+y=input()
+
+toplam=x+y
+print(toplam)#1020   input string değer olarak alıyor
+
+
+toplamm=int(x)+int(y)
+print(toplamm)
+
+
+##dairenin alanı ve çevresini bulma'''
+pi=3.14
+daireyaricapi=input('r')
+
+dairealani=pi*int(daireyaricapi)*int(daireyaricapi)
+dairecevresi=2*pi*int(daireyaricapi)
+
+print(dairealani)
+print(dairecevresi)
---
+++ python_temelleri/deneme2/variables.py
@@ -0,0 +1,37 @@
+x=1
+y=3
+name="bahar"
+
+a=10
+b=20
+print(a+b) #1020
+
+firstname="BAHAR"
+lastname="AYKUT"
+print(firstname+lastname) #BAHARAYKUT
+
+h,l,m,c=(1,1.2,"bahar",True)
+
+#örnek
+
+madi="bahar"
+msoyadi="aykut"
+madisoyadi=madi+msoyadi
+mcinsiyet="k"#true
+mtc="29507063158"
+mdogumyili=2004
+madresi="fhfjsfgf"
+myasi=2025-mdogumyili
+print(myasi)
+
+
+
+#örnek2  siparişlrerin toplam bilgisini hesapllayınız
+
+siparis1=110
+siparis2=1100.5
+siparis3=356.95
+
+toplamsiparis=siparis1+siparis2+siparis3
+
+print( "toplam",toplamsiparis)
